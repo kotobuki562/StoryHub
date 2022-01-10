991f3f13ac3e721116777cd4f56a00f6ad993222
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-01-10 14:38:01 +0900

    📝 登場人物のデータを作成する

---
+++ src/tools/characterCategory.ts
@@ -0,0 +1,58 @@
+export const characterCategory: Array<{
+  nameEn: string
+  nameJa: string
+  description: string
+}> = [
+  {
+    nameEn: "MainCharacter",
+    nameJa: "主人公",
+    description: "物語の中心人物であり主役的存在",
+  },
+  {
+    nameEn: "Heroine",
+    nameJa: "ヒロイン",
+    description: "物語の女主人公的存在",
+  },
+  {
+    nameEn: "Rival",
+    nameJa: "ライバル",
+    description: "物語の競争相手・好敵手",
+  },
+  {
+    nameEn: "Villian",
+    nameJa: "ヴィラン",
+    description: "物語の悪役。主人公と敵対する勢力",
+  },
+  {
+    nameEn: "Friend",
+    nameJa: "フレンド",
+    description: "主人公に対して友好的な関係を築く存在",
+  },
+  { nameEn: "Family", nameJa: "ファミリー", description: "主人公の肉親" },
+  {
+    nameEn: "Sidekick",
+    nameJa: "サイドキック",
+    description: "主人公の相棒的存在",
+  },
+  {
+    nameEn: "Mascot",
+    nameJa: "マスコット",
+    description: "物語に頻繁に出てくる小動物や象徴的な者",
+  },
+  {
+    nameEn: "Mob",
+    nameJa: "モブ",
+    description: "物語において中心的な存在ではないもの",
+  },
+  {
+    nameEn: "Mastermind",
+    nameJa: "マスターマインド(黒幕)",
+    description: "物語の黒幕・ボス的な存在",
+  },
+  {
+    nameEn: "Narrator",
+    nameJa: "語り部",
+    description: "物語を進めるだけの存在",
+  },
+  { nameEn: "Other", nameJa: "その他", description: "その他" },
+]
