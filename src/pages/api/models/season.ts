e636ccd08b862aa7a40c499c3d747a51695a7fd4
MaQueen <67810971+kotobuki562@users.noreply.github.com>
2022-01-17 22:26:32 +0900

    ✨ 設定資料にシーズンをリレーション

--- src/pages/api/models/season.ts
+++ src/pages/api/models/season.ts
@@ -8,6 +8,21 @@ const episodeArgs = {
   episodeUserId: nullable(stringArg()),
 }
 
+const characterArgs = {
+  characterAccessToken: nullable(stringArg()),
+  characterUserId: nullable(stringArg()),
+}
+
+const objectArgs = {
+  objectAccessToken: nullable(stringArg()),
+  objectUserId: nullable(stringArg()),
+}
+
+const terminologyArgs = {
+  terminologyAccessToken: nullable(stringArg()),
+  terminologyUserId: nullable(stringArg()),
+}
+
 const Season = objectType({
   name: "Season",
   definition(t) {
@@ -35,6 +50,75 @@ const Season = objectType({
               },
             })
           : prisma.episode.findMany({
+              orderBy: { created_at: "desc" },
+              where: {
+                season_id: `${parent.id}`,
+                publish: true,
+              },
+            })
+      },
+    })
+    t.list.field("characters", {
+      type: "Character",
+      args: characterArgs,
+      resolve: (parent, args) => {
+        const { characterAccessToken, characterUserId } = args
+        return characterAccessToken &&
+          characterUserId &&
+          isSafe(characterAccessToken, characterUserId)
+          ? prisma.character.findMany({
+              orderBy: { created_at: "desc" },
+              where: {
+                season_id: `${parent.id}`,
+              },
+            })
+          : prisma.character.findMany({
+              orderBy: { created_at: "desc" },
+              where: {
+                season_id: `${parent.id}`,
+                publish: true,
+              },
+            })
+      },
+    })
+    t.list.field("objects", {
+      type: "Object",
+      args: objectArgs,
+      resolve: (parent, args) => {
+        const { objectAccessToken, objectUserId } = args
+        return objectAccessToken &&
+          objectUserId &&
+          isSafe(objectAccessToken, objectUserId)
+          ? prisma.object.findMany({
+              orderBy: { created_at: "desc" },
+              where: {
+                season_id: `${parent.id}`,
+              },
+            })
+          : prisma.object.findMany({
+              orderBy: { created_at: "desc" },
+              where: {
+                season_id: `${parent.id}`,
+                publish: true,
+              },
+            })
+      },
+    })
+    t.list.field("terminologies", {
+      type: "Terminology",
+      args: terminologyArgs,
+      resolve: (parent, args) => {
+        const { terminologyAccessToken, terminologyUserId } = args
+        return terminologyAccessToken &&
+          terminologyUserId &&
+          isSafe(terminologyAccessToken, terminologyUserId)
+          ? prisma.terminology.findMany({
+              orderBy: { created_at: "desc" },
+              where: {
+                season_id: `${parent.id}`,
+              },
+            })
+          : prisma.terminology.findMany({
               orderBy: { created_at: "desc" },
               where: {
                 season_id: `${parent.id}`,
